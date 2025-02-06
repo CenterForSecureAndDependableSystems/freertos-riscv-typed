a9468cd0387680b82392f3a3977f6ca66d9eaae5
cartui <cart8305@vandals.uidaho.edu>
2025-02-06 19:37:42 +0000

    Add a new tag exception warn mode

--- libtypetag/include/typetag/control.h
+++ libtypetag/include/typetag/control.h
@@ -2,21 +2,15 @@
 #define TYPETAG_RISCV_H
 
 #include "typetag/typetag.h"
+#include "typetag/exception.h"
 
-// Enable or disable a particular tagging exception
-// when tt checks or propagation is enabled
-void tt_set_exception(tagexception_t exception, int enabled);
+/// Enable or disable a particular tagging exception
+/// when tt checks or propagation is enabled
+void tt_set_exception(tagexception_t exception, TrapMode mode);
 
-// TODO [TAG]: Need hint instruction. Necessary?
-// int tt_checks_enabled();
-
-// Enable or disable tag propagation
+/// Enable or disable tag propagation
 void tt_set_checks(int enabled);
-
-// TODO [TAG]: Need hint instruction. Necessary?
-// int tt_prop_enabled();
-
-// Enable or disable tag propagation
+/// Enable or disable tag propagation
 void tt_set_prop(int enabled);
 
 typetag_t tt_get_tag(char* ptr);
